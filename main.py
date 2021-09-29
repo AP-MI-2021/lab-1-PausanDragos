1de8fe629b4b0efd02a6e216aa9c2bb3dbb68a23
PausanDragos <dragos.pausan@yahoo.com>
2021-09-29 10:19:33 +0300

    am rezolvat tema

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
+import math
 '''
 Returneaza true daca n este prim si false daca nu.
 '''
 def is_prime(n):
-  # codul vostru aici
-  
+  for divisor in range(2, int(math.sqrt(n))):
+    if n%divisor==0:
+      return False
+  return True
   
 '''
 Returneaza produsul numerelor din lista lst.
 '''
 def get_product(lst):
-  # codul vostru aici
+  prod=1
+  for item in lst:
+    prod*=item
+  return prod
   
   
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
+  while y:
+    r=x%y
+    x=y
+    y=r
+  return x
   
   
 '''
@@ -24,10 +34,19 @@ Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
 def get_cmmdc_v2(x, y):
   # codul vostru aici
-  
+  return
   
 def main():
-  # interfata de tip consola aici
+  n = input('introduceti un numr: ')
+
+  if is_prime(int(n)):
+    print(n + ' e prim')
+  else:
+    print(n + ' nu e prim')
+
+  print(get_product([3,4,5]))
+
+  print(get_cmmdc_v1(10,5))
 
 if __name__ == '__main__':
   main()
